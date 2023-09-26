2c8b4efcf52b5a91c074d3e68245732e56e20d1c
亡灵暴龙大帝 <121159914+HongchengQ@users.noreply.github.com>
2023-09-26 08:15:35 +0800

    读取txt文件内容时去除注释行

--- io.py
+++ io.py
@@ -4,18 +4,24 @@ import json
 input_folder = "./input"
 output_folder = "./output"
 
-# 檢查輸出文件夾是否存在，如果不存在則創建
+# 检查输出文件夹是否存在，如果不存在则创建
 os.makedirs(output_folder, exist_ok=True)
 
-# 遍歷input文件夾中的所有txt文件
+# 遍历input文件夹中的所有txt文件
 for filename in os.listdir(input_folder):
     if filename.endswith('.txt'):
         txt_file_path = os.path.join(input_folder, filename)
         json_file_path = os.path.join(output_folder, filename.replace('.txt', '.json'))
 
-        # 讀取txt文件內容
+        # 读取txt文件内容并去除注释行
         with open(txt_file_path, 'r', encoding='utf-8') as file:
-            input_text = file.read()
+            lines = file.readlines()
+        
+        input_text = ""
+        for line in lines:
+            line = line.strip()
+            if line and not line.startswith("//"):
+                input_text += line + "\n"
 
         output_list = []
         lines = input_text.split('\n')
@@ -34,7 +40,7 @@ for filename in os.listdir(input_folder):
                 output_list.append(line_dict)
         output_text = json.dumps(output_list, indent=4, ensure_ascii=False)
 
-        # 將結果保存到json文件
+        # 将结果保存到json文件
         with open(json_file_path, 'w', encoding='utf-8') as file:
             file.write(output_text)
 
