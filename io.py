37e694301f44f7a4f94a79c5afcc699746497dd8
亡灵暴龙大帝 <121159914+HongchengQ@users.noreply.github.com>
2023-09-26 08:04:03 +0800

    Update io.py

--- io.py
+++ io.py
@@ -1,49 +1,41 @@
 import os
 import json
 
-# 指定要讀取的文件路徑和文件名
-file_path = "./input/武器.txt"
+input_folder = "./input"
+output_folder = "./output"
 
-# 檢查文件是否存在
-if os.path.isfile(file_path):
-    # 打開文件並讀取內容
-    with open(file_path, 'r', encoding='utf-8') as file:
-        input_text = file.read()
-else:
-    print("文件不存在：", file_path)
-    exit()
+# 檢查輸出文件夾是否存在，如果不存在則創建
+os.makedirs(output_folder, exist_ok=True)
 
-output_list = []
-lines = input_text.split('\n')
+# 遍歷input文件夾中的所有txt文件
+for filename in os.listdir(input_folder):
+    if filename.endswith('.txt'):
+        txt_file_path = os.path.join(input_folder, filename)
+        json_file_path = os.path.join(output_folder, filename.replace('.txt', '.json'))
 
-for line in lines:
-    if line.strip() != "":
-        parts = line.split(':')
-        value = int(parts[0])
-        label = parts[1]
+        # 讀取txt文件內容
+        with open(txt_file_path, 'r', encoding='utf-8') as file:
+            input_text = file.read()
 
-        line_dict = {
-            "label": label,
-            "value": value
-        }
+        output_list = []
+        lines = input_text.split('\n')
 
-        output_list.append(line_dict)
+        for line in lines:
+            if line.strip() != "":
+                parts = line.split(':')
+                value = int(parts[0])
+                label = parts[1]
 
-# 將 Python 對象轉換為格式化的 JSON 字符串，並禁用自動轉義
-output_text = json.dumps(output_list, indent=4, ensure_ascii=False)
+                line_dict = {
+                    "label": label,
+                    "value": value
+                }
 
-# 指定保存的文件路徑和文件名
-output_file_path = "./output/武器.json"
+                output_list.append(line_dict)
+        output_text = json.dumps(output_list, indent=4, ensure_ascii=False)
 
-# 檢查 output_text 是否為空
-if output_text:
-    # 創建目錄
-    os.makedirs(os.path.dirname(output_file_path), exist_ok=True)
+        # 將結果保存到json文件
+        with open(json_file_path, 'w', encoding='utf-8') as file:
+            file.write(output_text)
 
-    # 將文本保存到文件中
-    with open(output_file_path, 'w', encoding='utf-8') as file:
-        file.write(output_text)
-
-    print("文本已保存到文件：", output_file_path)
-else:
-    print("無需創建文件，輸出文本為空。")
+        print("文本已保存到文件：", json_file_path)
